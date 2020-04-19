51851ac01664e8817956cd8a51ae7e20b5170f62
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-04-19 03:23:47 -0700

    added a few pill boxes for the 5 day forcast as well as revamping the api call and uv index to log danger level

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-var cityInputEl = document.querySelector('#city')
-var savedCities = document.querySelector('.list-group')
+var cityInputEl = document.querySelector('#city');
+var savedCities = document.querySelector('.list-group');
+var cityDate = document.querySelector('#city-and-date');
+var cityTemp = document.querySelector('#temp');
+var cityHumid = document.querySelector('#humid');
+var cityWind = document.querySelector('#wind-speed');
+var cityUv = document.querySelector('#uv-index');
 var cityListId = 0;
 var citiesList = {};
 
@@ -23,8 +28,14 @@ var weatherapicall = function(userCity) {
     fetch(apiUrl).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-                createForecast(data);
-                getUvIndex(data);
+                var cityLon = data.city.coord.lon;
+                var cityLat = data.city.coord.lat;
+                var cityName = data.city.name
+                var tempCityDay = data.list[0].dt_txt.slice(8,10);
+                var tempCityMon = data.list[0].dt_txt.slice(5,7);
+                var tempCityY = data.list[0].dt_txt.slice(0,4)
+                cityDate.textContent = cityName + " " + tempCityMon + "/" + tempCityDay + "/" + tempCityY;
+                createForecast(cityLat, cityLon);
             });
         } else {
             alert('Error: ' + response.statusText)
@@ -32,19 +43,32 @@ var weatherapicall = function(userCity) {
     });
 }
 
-var createForecast = function(data) {
-
-}
-
-var getUvIndex = function(data) {
-    var cityLon = data.city.coord.lon;
-    var cityLat = data.city.coord.lat;
-    var uvApiUrl = 'http://api.openweathermap.org/data/2.5/uvi?appid=208b092a8fbdf8fceaf08ebd60cc31df&lat=' + cityLat +'&lon=' + cityLon;
-    fetch(uvApiUrl).then(function(response){
-        response.json().then(function(uvIndex) {
-            console.log(uvIndex);
+var createForecast = function(lat, lon) {
+    var dailyForCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=208b092a8fbdf8fceaf08ebd60cc31df'
+    fetch(dailyForCall).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data.daily[0]);
+            cityTemp.textContent = data.daily[0].temp.day;
+            cityHumid.textContent = data.daily[0].humidity;
+            cityWind.textContent = data.daily[0].wind_speed;
+            cityUv.textContent = data.daily[0].uvi;
+            if (data.daily[0].uvi > 8.0) {
+                cityUv.classList = "bg-danger text-light rounded p-1";
+            }
+            else if (data.daily[0].uvi > 3.0 && data.daily[0].uvi < 8.0) {
+                cityUv.classList = "bg-warning text-light rounded p-1";
+            }
+            else {
+                cityUv.classList = "bg-success text-light rounded p-1";
+            }
+            for (var i = 1; i < 6; i++) {
+                var tempTemp = document.querySelector('#temp-' + [i]);
+                var tempHumi = document.querySelector('#humid-' + [i]);
+                tempTemp.textContent = data.daily[i].temp.day
+                tempHumi.textContent = data.daily[i].humidity + "%"
+            }
         })
-    })
+    });
 }
 
 cityInputEl.addEventListener("submit", userInput);
