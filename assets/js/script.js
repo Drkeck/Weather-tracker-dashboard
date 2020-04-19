6d83def96a5d4503bb5a97249a06371ede7fe7f3
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-04-18 22:30:35 -0700

    added a second api call for the uv index based on first api call's city  cooridinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,24 +10,40 @@ var userInput = function(event) {
     weatherapicall(userCity);
 }
 
-var logInput = function(city) {
+var logInput = function(userCity) {
     var listEl = document.createElement("a");
     listEl.classList = "list-group-item text-dark"
-    listEl.setAttribute('href', './index.html?city=' + city);
-    listEl.textContent = city;
+    listEl.setAttribute('href', './index.html?city=' + userCity);
+    listEl.textContent = userCity;
     savedCities.appendChild(listEl);
 }
 
-var weatherapicall = function(city) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=208b092a8fbdf8fceaf08ebd60cc31df";
+var weatherapicall = function(userCity) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&units=imperial&appid=208b092a8fbdf8fceaf08ebd60cc31df";
     fetch(apiUrl).then(function(response){
         if (response.ok) {
             response.json().then(function(data){
-                console.log(data);
+                createForecast(data);
+                getUvIndex(data);
             });
         } else {
             alert('Error: ' + response.statusText)
         }
+    });
+}
+
+var createForecast = function(data) {
+
+}
+
+var getUvIndex = function(data) {
+    var cityLon = data.city.coord.lon;
+    var cityLat = data.city.coord.lat;
+    var uvApiUrl = 'http://api.openweathermap.org/data/2.5/uvi?appid=208b092a8fbdf8fceaf08ebd60cc31df&lat=' + cityLat +'&lon=' + cityLon;
+    fetch(uvApiUrl).then(function(response){
+        response.json().then(function(uvIndex) {
+            console.log(uvIndex);
+        })
     })
 }
 
